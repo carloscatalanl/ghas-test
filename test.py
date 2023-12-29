e89ad09123451a0b5714171a9389fa9032836e79
carloscatalanl <carlosacatalanlz@gmail.com>
2023-12-29 12:50:53 -0700

    Edit Workflow

--- test.py
+++
@@ -1,8 +0,0 @@
-# print("Dependabot for SCA testing")
-# print("CodeQL for SAST testing")
-
-import os
-
-directory = input("Enter the directory to list: ")
-command = f"ls {directory}"  # Vulnerable to Command Injection
-os.system(command)
