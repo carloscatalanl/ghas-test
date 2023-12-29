1e6ee11923810747e7c682adc7283d59aac7b09c
carloscatalanl <carlosacatalanlz@gmail.com>
2023-12-29 13:12:04 -0700

    Add vulnerability code

--- badcode.js
+++ badcode.js
@@ -1,12 +1,12 @@
 // Vulnerability code
 
-// function endsWith(x,y) {
+function endsWith(x,y) {
 
-//     let index = x.lastIndexOf(y);
-//     return x.lastIndexOf(y) === x.length - y.length;
+    let index = x.lastIndexOf(y);
+    return x.lastIndexOf(y) === x.length - y.length;
   
-//   }
+  }
 
 // Vulnerability code
 
-console.log("SCA and SAST");
+// console.log("SCA and SAST");
